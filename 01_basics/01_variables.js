17b9859fe6144f49395eb27e6a79362d8cf60c3e
Umair452 <120240458+Umair452@users.noreply.github.com>
2023-11-16 02:55:20 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const userId = 54 //const can only be declared with const keyword
+var userName = "Umair" 
+let userSurname = "Arshad"
+userCity = "Lalamusa" //it is also a variable
+
+accountAuth;
+
+/*not prefered method to declare with var because it does not follow block scope & func
+tional scope */
+
+console.table([userId,  userName,  userSurname, userCity, accountAuth])
