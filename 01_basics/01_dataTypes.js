f33c427fa210750e420e14019fa47bfe5f26e4e9
Umair452 <120240458+Umair452@users.noreply.github.com>
2023-11-16 02:55:01 +0000

    dataTypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //JS code will be treated as newer version, just a good practice :)
+
+
+// alert(3 + 3) it won't pop up because we are using node.js..
+
+
+let id = 54
+let userName = "Umair"
+let attendance = true
+let marks = null
+
+console.table([id, userName, attendance, marks ])
+
+/*Premitive Data types
+
+number=> 2 to power 53
+bigint bigger than number
+string => ""
+boolean => true / false
+null => standalone value //It shows its type of NULL as object
+undefined => when not defined anything but represents empty value
+symbol => to find uniqueness in components
+*/
+
+console.log(typeof null)
